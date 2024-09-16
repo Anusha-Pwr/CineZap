c584d3147814dc4aba03a3e6fcf2c01245e8d7b9
Anusha-Pwr <anusha00466@gmail.com>
2024-09-16 14:34:30 +0530

    Call api for image config in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useEffect } from "react";
-import { fetchDataFromApi } from "./utils/api";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import Home from "./pages/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
+import { fetchDataFromApi } from "./utils/api";
+import { useDispatch } from "react-redux";
+import { homeActions } from "./store/homeSlice";
 
 function App() {
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    async function apiTesting() {
-      const res = await fetchDataFromApi("/movie/popular");
+    async function fetchApiConfig() {
+      const res = await fetchDataFromApi("/configuration");
       console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original"
+      }
+
+      dispatch(homeActions.getApiConfiguration(url));
     }
 
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
   const router = createBrowserRouter([
--- src/pages/heroBanner/HeroBanner.jsx
+++ src/pages/heroBanner/HeroBanner.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const HeroBanner = () => {
 
@@ -7,6 +8,13 @@ const HeroBanner = () => {
   const [query, setQuery] = useState("");
 
   const navigate = useNavigate();
+
+  const {data, loading} = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg = data?.results[Math.floor(Math.random()*20)].backdrop_path;
+    setBackground(bg);
+  }, [data]);
 
   function searchQueryHandler(e) {
     if(e.key === "Enter" && query.length>0) {
