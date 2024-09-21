4a0d30a922411e22355acebd0b21c0d8996f3bce
Anusha-Pwr <anusha00466@gmail.com>
2024-09-21 12:08:01 +0530

    Add function fetchGenres to get genres for movies and tv shows

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,46 @@ import { useDispatch } from "react-redux";
 import { homeActions } from "./store/homeSlice";
 
 function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     async function fetchApiConfig() {
+      // function to get the configuration of images
       const res = await fetchDataFromApi("/configuration");
       console.log(res);
 
       const url = {
         backdrop: res.images.secure_base_url + "original",
         poster: res.images.secure_base_url + "original",
-        profile: res.images.secure_base_url + "original"
-      }
+        profile: res.images.secure_base_url + "original",
+      };
 
       dispatch(homeActions.getApiConfiguration(url));
     }
 
     fetchApiConfig();
+    fetchGenres();
   }, []);
+
+  async function fetchGenres() {
+    // function to get genres for movies and tv shows
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((endPoint) =>
+      promises.push(fetchDataFromApi(`/genre/${endPoint}/list`))
+    );
+
+    const data = await Promise.all(promises);
+    console.log(data);
+
+    data.forEach((genreObj) => {
+      genreObj.genres.forEach((genre) => (allGenres[genre.id] = genre));
+    });
+
+    dispatch(homeActions.getGenres(allGenres));
+  }
 
   const router = createBrowserRouter([
     {
